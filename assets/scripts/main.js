7eac9cfe30850aa6821af0b88b9d48e689f5dcf3
Abid <abid-elfargougui@outlook.fr>
2021-02-07 14:39:28 +0100

    init loader

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,10 +6,3 @@ setTimeout(function() {
     $(".loader-wrapper").fadeToggle();
     $('body').css('overflow','visible');
 }, 1600);
-
-function closeMenu(){
-    document.getElementById("navbar").style.height = "0%";
-}
-function openMenu(){
-    document.getElementById("navbar").style.height = "100%";
-}
