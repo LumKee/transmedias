4ad37e6226de77d159c0cf9a40454960993a628c
Abid <abid-elfargougui@outlook.fr>
2021-02-09 17:11:54 +0100

    update code

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,3 @@
-$(document).ready(function () {
-    $(this).scrollTop(0);
-});
-
 // setTimeout(function() {
 //     $(".loader-wrapper").fadeToggle();
 //     $('body').css('overflow','hidden');
@@ -10,24 +6,13 @@ $(document).ready(function () {
 let leafs = document.getElementById('Feuille');
 let leafsLeft = document.getElementById('FG');
 let leafsRight = document.getElementById('FD');
-
 let perspective = document.querySelector('.perspective');
-
-// function leafsOut() {
-//     leafs.style.cssText = 'transform: translateY(225px);';
-// };
-// leafs.addEventListener('mouseover', leafsOut, false);
 
 function leafsOut() {
     leafsLeft.style.cssText = 'transform: translateX(-225px) translateY(200px);';
     leafsRight.style.cssText = 'transform: translateX(225px) translateY(200px);';
 };
 leafs.addEventListener('mouseover', leafsOut, false);
-
-// function leafsIn() {
-//     leafs.style.cssText = 'transform: translateY(0px);';
-// };
-// leafs.addEventListener('mouseleave', leafsIn, false);
 
 function leafsIn() {
     leafsLeft.style.cssText = 'transform: translateX(0px) translateY(0px);';
