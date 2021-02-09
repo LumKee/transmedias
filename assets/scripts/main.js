e450f3ac40549969f314a9abab34f81f7517b6f9
Abid <abid-elfargougui@outlook.fr>
2021-02-09 15:33:14 +0100

    update code

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,21 +2,36 @@ $(document).ready(function () {
     $(this).scrollTop(0);
 });
 
-setTimeout(function() {
-    $(".loader-wrapper").fadeToggle();
-    $('body').css('overflow','hidden');
-}, 3600);
+// setTimeout(function() {
+//     $(".loader-wrapper").fadeToggle();
+//     $('body').css('overflow','hidden');
+// }, 3600);
 
 let leafs = document.getElementById('Feuille');
+let leafsLeft = document.getElementById('FG');
+let leafsRight = document.getElementById('FD');
+
 let perspective = document.querySelector('.perspective');
 
+// function leafsOut() {
+//     leafs.style.cssText = 'transform: translateY(225px);';
+// };
+// leafs.addEventListener('mouseover', leafsOut, false);
+
 function leafsOut() {
-    leafs.style.cssText = 'transform: translateY(225px);';
+    leafsLeft.style.cssText = 'transform: translateX(-225px) translateY(200px);';
+    leafsRight.style.cssText = 'transform: translateX(225px) translateY(200px);';
 };
 leafs.addEventListener('mouseover', leafsOut, false);
 
+// function leafsIn() {
+//     leafs.style.cssText = 'transform: translateY(0px);';
+// };
+// leafs.addEventListener('mouseleave', leafsIn, false);
+
 function leafsIn() {
-    leafs.style.cssText = 'transform: translateY(0px);';
+    leafsLeft.style.cssText = 'transform: translateX(0px) translateY(0px);';
+    leafsRight.style.cssText = 'transform: translateX(0px) translateY(0px);';
 };
 leafs.addEventListener('mouseleave', leafsIn, false);
 
