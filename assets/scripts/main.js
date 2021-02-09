62a91c9f60a2b1d8d20b19ea6ef5436f8e36b40d
Abid <abid-elfargougui@outlook.fr>
2021-02-09 13:23:10 +0100

    add travelling & leafs move effect

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,8 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $(this).scrollTop(0);
 });
 
-// setTimeout(function() {
-//     $(".loader-wrapper").fadeToggle();
-//     $('body').css('overflow','hidden');
-// }, 3600);
+setTimeout(function() {
+    $(".loader-wrapper").fadeToggle();
+    $('body').css('overflow','hidden');
+}, 3600);
+
+let leafs = document.getElementById('Feuille');
+let perspective = document.querySelector('.perspective');
+
+function leafsOut() {
+    leafs.style.cssText = 'transform: translateY(225px);';
+};
+leafs.addEventListener('mouseover', leafsOut, false);
+
+function leafsIn() {
+    leafs.style.cssText = 'transform: translateY(0px);';
+};
+leafs.addEventListener('mouseleave', leafsIn, false);
+
+function travelling() {
+    perspective.style.animationPlayState = 'running';
+};
+leafs.addEventListener('click', travelling, false);
