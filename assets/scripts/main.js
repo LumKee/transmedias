a6fa601bbc0b2f65bfb7669c72ff5e2d96907168
Abid <abid-elfargougui@outlook.fr>
2021-02-11 19:49:05 +0100

    update loader screen

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,7 @@
-setTimeout(function() {
-    $(".loader-wrapper").fadeToggle();
-    $('body').css('overflow','hidden');
-}, 3600);
+// setTimeout(function() {
+//     $(".loader-wrapper").fadeToggle();
+//     $('body').css('overflow','hidden');
+// }, 3600);
 
 let leafs = document.getElementById('Feuille');
 let leafsLeft = document.getElementById('FG');
@@ -9,6 +9,13 @@ let leafsRight = document.getElementById('FD');
 let entrance = document.getElementById('grotteEntrance_1_')
 let perspective = document.querySelector('.perspective');
 let webgl = document.querySelector('.webgl-content');
+let launchBtn = document.querySelector('.launch-btn');
+
+function launchExp() {
+    $(".loader-wrapper").fadeToggle();
+    $('body').css('overflow','hidden');
+};
+launchBtn.addEventListener('click', launchExp, false);
 
 function leafsOut() {
     leafsLeft.classList.add("left-transition");
@@ -26,10 +33,10 @@ leafs.addEventListener('mouseover', leafsOut, false);
 
 function travelling() {
     perspective.style.animationPlayState = 'running';
-    
+
     setTimeout(function () {
         perspective.style.display = 'none';
-        webgl.style.display='block';
+        webgl.style.display = 'block';
     }, 1200);
 };
 entrance.addEventListener('click', travelling, false);
